d17cf8d881de9f4d5d62e6a557d3a5471279d646
Ankita <ankita@gmail.com>
2019-10-14 21:14:07 -0700

    main in dbda1

---
+++ main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Calculator
+{
+  public static void main(String[] args)
+{
+	Scanner sr=new Scanner(System.in);
+        System.out.println("Enter 2 numbers");
+	double num1=sr.nextDouble();
+	double num2=sr.nextDouble();
+	System.out.println("Enter your command");
+  	String operation=sr.next();
+        boolean  exit=false;
+
+}
+}
