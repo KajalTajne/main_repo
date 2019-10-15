5720278fc3c796b33979fde772aa0b90c60c53ce
Ankita <ankita@gmail.com>
2019-10-14 21:26:06 -0700

    new main in master

--- main.java
+++ main.java
@@ -1,39 +1,7 @@
-import java.util.Scanner;
-
 class Calculator
 {
   public static void main(String[] args)
 {
-	Scanner sr=new Scanner(System.in);
-        System.out.println("Enter 2 numbers");
-	double num1=sr.nextDouble();
-	double num2=sr.nextDouble();
-	System.out.println("Enter your command");
-  	String operation=sr.next();
-        boolean  exit=false;
         System.out.println("hello kajal");
-<<<<<<< HEAD
-while(!exit)
-{
-	switch(operation)
-	{
-	
-		case "add":System.out.println(num1+num2);
-			break;
-	        case "sub":System.out.println(num1-num2);
-		         break;
-		case "mul":System.out.println(num1*num2);
-		         break;
-                case "div":System.out.println(num1/num2);
-		         break;
-      		case "quit":exit=false;
-			 break;
-
-
-	}
-}
-
-=======
->>>>>>> dbda1
 }
 }
