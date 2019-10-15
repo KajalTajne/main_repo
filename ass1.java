11ca79be8fbd29a17013d33379bcd4c1f3cf502b
Ankita <ankita@gmail.com>
2019-10-14 20:47:50 -0700

    first commit

---
+++ ass1.java
@@ -0,0 +1,36 @@
+ner;
+
+class Calculator
+{
+  public static void main(String[] args)
+{
+	Scanner sr=new Scanner(System.in);
+        System.out.println("Enter 2 numbers");
+	double num1=sr.nextDouble();
+	double num2=sr.nextDouble();
+	System.out.println("Enter your command");
+  	String operation=sr.next();
+        boolean  exit=false;
+
+while(!exit)
+{
+	switch(operation)
+	{
+	
+		case "add":System.out.println(num1+num2);
+			break;
+	        case "sub":System.out.println(num1-num2);
+		         break;
+		case "mul":System.out.println(num1*num2);
+		         break;
+                case "div":System.out.println(num1/num2);
+		         break;
+      		case "quit":exit=false;
+			 break;
+
+
+	}
+}
+
+}
+}
